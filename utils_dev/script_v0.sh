5ea0c7fbb90f87f6cf52437a4b7109a7ea37f5bc
Pauline Turk <pauline@abizarre-2.local>
2022-05-04 14:53:31 +0200

    cubeGenerator.py RENAME

--- utils_dev/script_v0.sh
+++ utils_dev/script_v0.sh
@@ -16,14 +16,19 @@
 #SBATCH --partition=type_2
 
 # Name of output file
-#SBATCH --output=test_1.out
+#SBATCH --output=cubes_10.out
 
 # Calculation times
-#SBATCH --time=00-02:00:00
+#SBATCH --time=1-00:00:00
+
+# Add email
+#SBATCH --mail -type=ALL
+#SBATCH --mail -user=pauline.turk@agroparistech.fr
+
 
 # loading modules
 module load userspace/tr17.10
-modue load python/3.6.3
+module load python/3.6.3
 
 
 
@@ -34,7 +39,7 @@ echo -e "\nStart calcul cubs\n"
 
 path_folder_pID=(/mnt/beegfs/pturk/PID_couple)
 path_folder_data_split=(/mnt/beegfs/pturk/PfamSplit_50)
-path_new_folder=(/mnt/beegfs/pturk/test_v1)
+path_new_folder=(/mnt/beegfs/pturk/Cubes_10_Pfam_50_A)
 
 cd /trinity/home/pturk
 python ./scriptCubs.py $path_folder_pID $path_folder_data_split $path_new_folder
